ecf7db04ed7375d997488c3c76dd105495254a6c
maybesomesense <228cacka228@mail.ru>
2022-04-11 13:58:06 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/treeExample.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/treeExample.dir/petclub.c.obj"
+  "CMakeFiles/treeExample.dir/tree.c.obj"
+  "treeExample.exe"
+  "treeExample.exe.manifest"
+  "treeExample.lib"
+  "treeExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/treeExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
